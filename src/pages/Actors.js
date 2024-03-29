627d60610bc88f9347af182cada42ac61c4042dd
Jazper <jazpersaldana@yahoo.com>
2024-03-29 15:04:24 -0500

    react router lab complete

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar'
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/actors')
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const actorDisplay = actors.map(actor => {
+    const movieList = actor.movies.map(movie => (
+      <li key={movie}>{movie}</li>
+    ))
+
+    return (
+      <article key={actor.id}>
+      <h2>{actor.name}</h2>
+      <h4>Movies:</h4>
+      <ul>{movieList}</ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorDisplay}
       </main>
     </>
   );
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar'
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/directors')
+    .then(r => r.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const directorDisplay = directors.map(director => {
+    const moviesList = director.movies.map(movie => (
+      <li key={movie}>{movie}</li>
+    ))
+
+    return (
+    <article key={director.id}>
+      <h2>{director.name}</h2>
+      <h4>Movies:</h4>
+      <ul>{moviesList}</ul>
+    </article>
+  )})
+  
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorDisplay}
       </main>
     </>
   );
